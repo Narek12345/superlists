ab419405f5f005f3f350f6b8a4edd0e1bea8e365
Narek <narekbayanduryan16@gmail.com>
2023-11-12 19:48:05 +0300

    Progect update.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -18,6 +18,13 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 
+	def check_for_row_in_list_table(self, row_text):
+		"""Подтверждение строки в таблице списка."""
+		table = self.browser.find_element(By.ID, 'id_list_table')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		"""Тест: можно добавить элемент в список и затем его получить."""
 		# Эдит слышала про крутое новое онлайн-приложение со списком неотложенных дел. Она решает оценить его домашнюю страницу.
@@ -41,10 +48,7 @@ class NewVisitorTest(unittest.TestCase):
 		# Когда она нажимает enter, страница обновляется, и теперь страница содержит "1: Купить павлиньи перья" в качестве элемента списка.
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Купить павлиньи перья')
 		
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит "Сделать мушку из павлиньих перьев".
 		# (Эдит очень методична).
@@ -54,10 +58,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 		
 		# Страница снова обновляется, и теперь показывает оба элемента ее списка.
-		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-		self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Купить павлиньи перья')
+		self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 		
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт сгенерировал для нее уникальный URL-адрес - об этом выводится небольшой текст с обьяснением.
 		self.fail('Закончить тест!')
