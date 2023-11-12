6163e6889f99871d5c9d2c9c5fba8e7c98abb1eb
Narek <narekbayanduryan16@gmail.com>
2023-11-12 19:40:18 +0300

    Adding a unittest to validate a form.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -44,18 +44,23 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element(By.ID, 'id_list_table')
 		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertTrue(
-			any(row.text == '1: Купить павлиньи перья' for row in rows),
-			'Новый элемент списка не появился в таблице'
-		)
+		self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 		
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит "Сделать мушку из павлиньих перьев".
 		# (Эдит очень методична).
-		self.fail('Закончить тест!')
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		inputbox.send_keys('Сделать мушку из павлиньих перьев')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 		
 		# Страница снова обновляется, и теперь показывает оба элемента ее списка.
+		table = self.browser.find_element(By.ID, 'id_list_table')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
+		self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+		self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
 		
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт сгенерировал для нее уникальный URL-адрес - об этом выводится небольшой текст с обьяснением.
+		self.fail('Закончить тест!')
 		
 		# Она посещает этот URL-адрес - ее список по-прежнему там.
 		
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -13,3 +13,9 @@ class HomePageTest(TestCase):
 		"""Тест: используется домашний шаблон."""
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		"""Тест: можно сохранить post-запрос."""
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
