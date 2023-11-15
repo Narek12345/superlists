ef9bdfb2cf4d8af3ba71961aea75419d7b72900a
Narek <narekbayanduryan16@gmail.com>
2023-11-15 12:18:23 +0300

    Progect update.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -18,11 +21,19 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.quit()
 
 
-	def check_for_row_in_list_table(self, row_text):
-		"""Подтверждение строки в таблице списка."""
-		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		"""Ожидать строку в таблице списка."""
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element(By.ID, 'id_list_table')
+				rows = table.find_elements(By.TAG_NAME, 'tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
@@ -47,19 +58,17 @@ class NewVisitorTest(LiveServerTestCase):
 		
 		# Когда она нажимает enter, страница обновляется, и теперь страница содержит "1: Купить павлиньи перья" в качестве элемента списка.
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Купить павлиньи перья')
+		self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 		
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит "Сделать мушку из павлиньих перьев".
 		# (Эдит очень методична).
 		inputbox = self.browser.find_element(By.ID, 'id_new_item')
 		inputbox.send_keys('Сделать мушку из павлиньих перьев')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 		
 		# Страница снова обновляется, и теперь показывает оба элемента ее списка.
-		self.check_for_row_in_list_table('1: Купить павлиньи перья')
-		self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+		self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+		self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 		
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт сгенерировал для нее уникальный URL-адрес - об этом выводится небольшой текст с обьяснением.
 		self.fail('Закончить тест!')
