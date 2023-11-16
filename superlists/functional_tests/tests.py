12f1b6bb2fb561d57dbcadc8c04ad1efc25ab6fc
Narek <narekbayanduryan16@gmail.com>
2023-11-16 16:22:08 +0300

    The first steps of FT in testing style design.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -117,5 +117,31 @@ class NewVisitorTest(LiveServerTestCase):
 		# Удовлетворенные, они обы ложатся спать.
 
 
+	def test_layout_and_styling(self):
+		"""Тест макета и стилевого оформления."""
+		# Эдит открывает домашнюю страницу.
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# Она замечает, что поле ввода аккуратно центрировано.
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
+		# Она начинает новый список и видит, что поле ввода тоже находится по центру.
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
